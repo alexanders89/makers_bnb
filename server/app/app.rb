fb93df40fadf4e1a1abc3021f2560c1ca6001cf9
Alex <alex.satur89@gmail.com>
2017-06-12 17:23:31 +0100

    WIP first user story implementation

--- server/app/app.rb
+++ server/app/app.rb
@@ -1,11 +1,18 @@
 require 'sinatra/base'
+require_relative 'datamapper_setup'
 
 class MakersBnb < Sinatra::Base
 
   get '/' do
-    'Hello World'
+    erb :signup
   end
 
-  run! if app_file == $0
+  post '/listings' do
+    'Thanks for signing up!'
+
+  end
+
+
+run! if app_file == $0
 
 end
