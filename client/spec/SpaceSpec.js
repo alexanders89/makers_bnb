f5d3bb94e2b323e08598a483ccf4604973d6a384
bannastre <bannastre@gmail.com>
2017-06-13 14:24:25 +0100

    Space function has a description - user can edit their descritpion - user can edit their name

--- client/spec/SpaceSpec.js
+++ client/spec/SpaceSpec.js
@@ -8,7 +8,21 @@ describe('Space', function() {
   });
 
   it('knows its name', function() {
-    expect(space.name).toEqual("My new space");
+    expect(space.name).toEqual('My new space');
+  });
+
+  it('is able to change name', function(){
+    space.editName('My updated space name');
+    expect(space.name).toEqual('My updated space name');
+  });
+
+  it('has a description', function(){
+    expect(space.description).toEqual('Add details about your space here.');
+  });
+
+  it('is able to change description', function(){
+    space.editDescription('My updated space description');
+    expect(space.description).toEqual('My updated space description');
   });
 
 });
--- client/src/space.js
+++ client/src/space.js
@@ -1,3 +1,12 @@
 function Space() {
   this.name = "My new space"
+  this.description = "Add details about your space here."
+}
+
+Space.prototype.editName = function(name){
+  this.name = name
+}
+
+Space.prototype.editDescription = function(description){
+  this.description = description
 }
