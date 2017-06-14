c7cb4ef6643b9d504cb98df06f3f25540a8d6453
IAN SIMPSON <ianconradsimpson@gmail.com>
2017-06-14 18:21:42 +0100

    added list of spaces

--- client/src/spaces_interface.js
+++ client/src/spaces_interface.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   var space = new Space();
   var server = 'http://localhost:9292'
+    listSpaces();
 
   $('#space-add').on('click', function(){
     name = document.getElementById("space-name").value;
@@ -12,6 +13,17 @@ $(document).ready(function() {
 
   function storeNewSpace() {
     $.post(server + '/spaces/new', {"name": space.name, "description": space.description});
+  }
+
+
+    function listSpaces() {
+      $.get('http://localhost:9292/spaces', function (data){
+        $.each(data, function(data) {
+          alert(data.name)
+        });
+        $('#space-list').text(data);
+        console.log(data)
+      });
   }
 
 });
