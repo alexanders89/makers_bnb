5e03c930d5a820d69e72cc9e16ec2bd10941a088
IAN SIMPSON <ianconradsimpson@gmail.com>
2017-06-15 13:37:39 +0100

    Included JQuery/ajax so names and spaces can be listed on html page

--- client/src/spaces_interface.js
+++ client/src/spaces_interface.js
@@ -18,12 +18,32 @@ $(document).ready(function() {
 
     function listSpaces() {
       $.get('http://localhost:9292/spaces', function (data){
-        $.each(data, function(data) {
-          alert(data.name)
-        });
-        $('#space-list').text(data);
-        console.log(data)
+        dataType: 'String';
+        // var spaces = data
+        // var space = spaces.map(JSON.parse);
+        // console.log(space);
+        var spaces = JSON.parse(data);
+        // var space = id.name.description
+        // for (var space in spaces) {
+        //   console.log();
+        // }
+        // for (i = 0; i < spaces.length; i++) {
+        //   $('space-list').text(spaces[i]);
+        // }
+
+    //     $('#space-list').text(function() {
+    //       for (i = 0; i < spaces.length; i++) {
+    //      spaces[0]['name'];
+    //    }
+    //  });
+        console.log(spaces[0]);
+        console.log(spaces[1]);
+
+        for (i = 0; i < spaces.length; i++) {
+          // $('#space-list').append(spaces[0]);
+          $('ul').append('<li>'+ "NAME: " + spaces[i].name + ": " + "SPACE: " + spaces[i].description + '</li>');
+        }
       });
-  }
+    }
 
 });
