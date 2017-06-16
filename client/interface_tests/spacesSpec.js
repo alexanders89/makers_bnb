e46a489f29696fa2a4928e38eddea1630ada1ab8
Alex <alex.satur89@gmail.com>
2017-06-16 16:33:05 +0100

    merge html pages

--- client/interface_tests/spacesSpec.js
+++ client/interface_tests/spacesSpec.js
@@ -2,6 +2,8 @@ var addNewSpace = function () {
   var button = $('#add-space');
   button.click();
   window.open('new_spaces.html')
+  console.log($('#space-new').text());
+  console.log('Hi mu');
   return $('#space-new').text() ==='Space Name:';
 }
 
